ea9c26be8472e620badf88ed103d3fa79b1ee855
Sebastian Civarolo <sebastian@coredesignstudio.com>
2018-06-11 16:11:01 -0500

    Adds Typekit code

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+  <link rel="stylesheet" href="https://use.typekit.net/ujf7gzi.css">
 	<?php wp_head(); ?>
 </head>
 
