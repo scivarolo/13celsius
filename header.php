26277b5ae111dad5db6b7687a200bdc3e70a5758
Sebastian Civarolo <sebastian@coredesignstudio.com>
2018-07-05 17:14:23 -0500

    Initial styles for header.

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'celsius' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'celsius' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
